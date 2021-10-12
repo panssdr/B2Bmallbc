c948d3a2ef71c2a2728fc38c1558cb8fcc02cead
panssdr <524105236@qq.com>
2021-10-12 18:31:02 +0800

    first commit

---
+++ src/main/java/com/usx/b2bmall/B2bmallApplication.java
@@ -0,0 +1,13 @@
+package com.usx.b2bmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2bmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B2bmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/usx/b2bmall/B2bmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usx.b2bmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2bmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

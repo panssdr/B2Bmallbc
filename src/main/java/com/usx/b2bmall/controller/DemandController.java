b83fa97d1a85b593db880e550eecc620003b19da
panssdr <524105236@qq.com>
2021-10-14 12:06:12 +0800

    demand data

--- src/main/java/com/usx/b2bmall/controller/DemandController.java
+++ src/main/java/com/usx/b2bmall/controller/DemandController.java
@@ -1,9 +1,17 @@
 package com.usx.b2bmall.controller;
 
 
+import com.usx.b2bmall.mapper.DemandMapper;
+import com.usx.b2bmall.pojo.Demand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,9 +21,17 @@ import org.springframework.stereotype.Controller;
  * @author Panshenshen
  * @since 2021-10-13
  */
-@Controller
+@RestController
+@ResponseBody
 @RequestMapping("/demand")
 public class DemandController {
 
+    @Autowired
+    private DemandMapper demandMapper;
+
+    @GetMapping("/all")
+    public List<Demand> demandAll(){
+        return demandMapper.getAll();
+    }
 }
 
--- src/main/java/com/usx/b2bmall/mapper/DemandMapper.java
+++ src/main/java/com/usx/b2bmall/mapper/DemandMapper.java
@@ -1,8 +1,11 @@
 package com.usx.b2bmall.mapper;
 
-import com.usx.b2bmall.pojo.Demand;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.usx.b2bmall.pojo.Demand;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,7 +15,13 @@ import org.apache.ibatis.annotations.Mapper;
  * @author Panshenshen
  * @since 2021-10-13
  */
+
 @Mapper
 public interface DemandMapper extends BaseMapper<Demand> {
 
+
+    @Select("SELECT ID id,GoodsName goodsName,Specifications specifications," +
+            "Price price,Amount amount,CreateDate createDate,Unit unit," +
+            "(SELECT category.`Name` FROM category where demand.CategoryID1= category.ID )as type from demand")
+     List<Demand> getAll();
 }

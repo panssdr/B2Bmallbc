ef327ee4ab91f76e62a5c7797c4676fdfb94cb48
panssdr <524105236@qq.com>
2021-10-15 00:21:40 +0800

    登陆有问题

--- src/main/java/com/usx/b2bmall/controller/CustomerController.java
+++ src/main/java/com/usx/b2bmall/controller/CustomerController.java
@@ -1,9 +1,15 @@
 package com.usx.b2bmall.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.usx.b2bmall.pojo.Customer;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.stereotype.Controller;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * <p>
@@ -14,8 +20,14 @@ import org.springframework.stereotype.Controller;
  * @since 2021-10-13
  */
 @Controller
+@ResponseBody
 @RequestMapping("/customer")
 public class CustomerController {
-
+    
+   @PostMapping("/register")
+    public void registerCustomer(Map<String,String> jsonString){
+       System.out.println(jsonString.get("cotelephone"));
+       System.out.println(jsonString.get("password"));
+    }
 }
 
